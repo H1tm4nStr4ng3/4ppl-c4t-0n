b3e5f974196f6d987cac549c594e35ec2d8a0a70
H1tm4nStr4ng3 <vladimirurquiola.telecom@gmail.com>
2026-01-30 22:57:18 -0400

    Delete backend directory

--- backend/app/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Schemas Pydantic - Importación de todos los schemas
-"""
-from app.schemas.base import *
-from app.schemas.all_schemas import *
-
-__all__ = ["PaginatedResponse", "MessageResponse", "ErrorResponse"]
